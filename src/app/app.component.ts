1d30a68113cb0237f82e77e5112aa9cb4b38fb98
kokej <jamarqueza@gmail.com>
2020-03-03 18:19:16 +0100

    cutrely

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const grammar: any;
+declare const nearley: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nearly';
+
+  ngOnInit(): void {
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    parser.feed('foo\n');
+    console.log(parser.results);
+  }
 }
